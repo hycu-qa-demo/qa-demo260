cc6557f228d7ef2240ca0d10c3a7586ab6c97ad2
kurdush-oss <kurdush_T@ycqdz.onmicrosoft.com>
2026-06-07 22:48:22 +0530

    QA populate: src/utils/helpers.py

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,3 +1,15 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8d80487df83cc9203d836c26862b44fae47dacb1aec2bd1a77bedbd03953933b
-size 268
+"""QA utility helpers."""
+
+
+def greet(name: str) -> str:
+    message = f"Hello, {name}!"
+    print(message)
+    return message
+
+
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+def slugify(text: str) -> str:
+    return text.lower().replace(" ", "-")
